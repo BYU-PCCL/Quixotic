af771232f9e6b872385a1b6378b1bf0774b7ab38
Iris Seaman <irisrseaman@gmail.com>
2017-02-16 18:20:46 -0700

    updating data_gathering.py

--- viewer/data_gathering.py
+++ viewer/data_gathering.py
@@ -43,14 +43,17 @@ def Update():
 		    print pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1],
 		    return pygame.mouse.get_pos()
 
-def load_polygons_here( fn="./paths.txt" ):
+def load_polygons_here( fn="./info_gathering_paths.txt" ):
     bdata = []
+    count = 1
     for x in open( fn ):
-        tmp = np.fromstring( x, dtype=float, sep=' ' )
-        tmp = np.reshape( tmp/1000, (-1,2) )
-        tmp = np.vstack(( np.mean(tmp, axis=0, keepdims=True), tmp, tmp[0,:] ))
-        #tmp[:,1] = 1.0 - tmp[:,1]  # flip on the y axis
-        bdata.append( tmp )
+    	if count != 0:
+	        tmp = np.fromstring( x, dtype=float, sep=' ' )
+	        tmp = np.reshape( tmp/500, (-1,2) )
+	        tmp = np.vstack(( np.mean(tmp, axis=0, keepdims=True), tmp, tmp[0,:] ))
+	        #tmp[:,1] = 1.0 - tmp[:,1]  # flip on the y axis
+	        bdata.append( tmp )
+       	count += 1
     return bdata
 
 def load_polygons( fn="./info_gathering_paths.txt" ):
@@ -91,6 +94,87 @@ def load_polygons( fn="./info_gathering_paths.txt" ):
 	# 	print "\n", p
 	return polygonSegments
 
+def IsIntruderSeen(RRTPath, intersections):
+
+        for point in RRTPath:
+            isFound = FindIntruderAtPoint(point, intersections)
+            if isFound:
+                return True
+
+        for i in xrange(1,len(RRTPath)):
+            segment = (RRTPath[i-1], RRTPath[i])
+
+            for j in xrange(1,len(intersections)):
+            	isovist_segment = (intersections[j-1], intersections[j])
+            	intersect, param = GetIntersection(segment, isovist_segment)
+            	if intersect != None:
+            		return True
+            #Check the losing of the polygon segment
+            
+            isovist_segment = (intersections[0], intersections[-1])
+            intersect, param = GetIntersection(segment, isovist_segment)
+            if intersect != None:
+            	return True
+
+        return False
+
+def FindIntruderAtPoint(pos, intersections):
+        if intersections == []:
+            return False
+        points = intersections
+        cn = 0  # the crossing number counter
+        pts = points[:]
+        pts.append(points[0])
+        for i in range(len(pts) - 1):
+            if (((pts[i][1] <= pos[1]) and (pts[i+1][1] > pos[1])) or ((pts[i][1] > pos[1]) and (pts[i+1][1] <= pos[1]))):
+                    if (pos[0] < pts[i][0] + float(pos[1] - pts[i][1]) / (pts[i+1][1] - pts[i][1]) * (pts[i+1][0] - pts[i][0])):
+                            cn += 1
+        if bool(cn % 2)==1:
+            return True
+        return False
+
+def GetIntersection(ray, segment):
+        # RAY in parametric: Point + Direction * T1
+        r_px = ray[0][0]
+        r_py = ray[0][1]
+
+        # direction
+        r_dx = ray[1][0] - ray[0][0]
+        r_dy = ray[1][1] - ray[0][1]
+
+        # SEGMENT in parametric: Point + Direction*T2
+        s_px = segment[0][0]
+        s_py = segment[0][1]
+
+        # direction
+        s_dx = segment[1][0] - segment[0][0]
+        s_dy = segment[1][1] - segment[0][1]
+
+        r_mag = math.sqrt(r_dx ** 2 + r_dy ** 2)
+        s_mag = math.sqrt(s_dx ** 2 + s_dy ** 2)
+
+        if r_mag == 0 or s_mag == 0:
+        	return None, None
+        # PARALLEL - no intersection
+        if (r_dx/r_mag) == (s_dx/s_mag):
+            if (r_dy/r_mag) == (s_dy/s_mag):
+                return None, None
+        
+        denominator = float( -s_dx*r_dy + r_dx*s_dy )
+        if denominator == 0:
+            return None, None
+
+        T1 = (-r_dy * (r_px - s_px) + r_dx * ( r_py - s_py)) / denominator
+        T2 = (s_dx * ( r_py - s_py) - s_dy * ( r_px - s_px)) / denominator
+
+        if T1 >= 0 and T1 <= 1 and T2 >= 0 and T2 <= 1:
+            #Return the POINT OF INTERSECTION
+            x = r_px+r_dx*T2
+            y = r_py+r_dy*T2
+            param = T2
+            return ( x, y ), param
+
+        return None, None
 
 '''
 	main function
@@ -128,14 +212,14 @@ def main():
 
 	#screen.blit(background, backgroundRect)
 
-
+	#101 455 336 47
 	#### RRT STUFF
-	# start_paint = (int(0.1 *500),int(0.1 *500))
-	# end_paint = (int(0.9*500), int(0.9 *500))
+	start_paint = (int(101),int(455))
+	end_paint = (int(336), int(47))
 
-	# start = np.atleast_2d( [(0.1 ) ,(0.1 )] )
-	# end = np.atleast_2d( [(0.9 ),(0.9 )] )
-	# X1, Y1, X2, Y2 = polygons_to_segments(load_polygons_here())
+	start = np.atleast_2d( [(101 /500.0 ) ,(455/500.0 )] ) #(101, 455)
+	end = np.atleast_2d( [(336/500.0 ),(47/500.0)] ) #(336, 47)
+	X1, Y1, X2, Y2 = polygons_to_segments(load_polygons_here())
 
 	# Draw segments
 	for polygon in polygonSegments:
@@ -182,27 +266,33 @@ def main():
 		diry = mouse[1] - UAVLocation[1]
 		direction = (dirx, diry)
 
-		#Draw hard coded RRT Path
-		# pygame.draw.line(screen, (0, 0, 255), RRTPath[0], RRTPath[1],2)
-		# for point in RRTPath:
-		# 	pygame.draw.circle(screen, (255,100,255), point, 5)
-
-		#pygame.draw.circle(screen, (255,255,255), start_paint, 15)
-		#pygame.draw.circle(screen, (255,255,255), end_paint, 15)
-
-		# pygame.draw.circle(screen, (0,255,0), start_paint, 10)
-		# pygame.draw.circle(screen, (255,0,0), end_paint, 10)
+		pygame.draw.circle(screen, (0,255,0), start_paint, 7)
+		pygame.draw.circle(screen, (255,0,0), end_paint, 7)
 		
 		#UAVForwardVector = direction
 		UAVForwardVector = (2, 121)
-		#print UAVForwardVector
+
+		path = run_rrt( start, end, X1, Y1, X2, Y2)
+		readable_path = []
+		for i in xrange(1, len(path)):
+			s_point = path[i-1]
+			s_point = (int(s_point[0]*500), int(s_point[1]*500))
+
+			e_point = path[i]
+			e_point = (int(e_point[0]*500), int(e_point[1]*500))
+			pygame.draw.line(screen, (0, 0, 255), s_point, e_point, 2)
+
+			readable_path.append(s_point)
+		pygame.draw.line(screen, (0, 0, 255), e_point, end_paint, 2)
 
 		#isIntruderFound, intersections = isovist.IsIntruderSeen([mouse], UAVLocation, UAVForwardVector, UAVFieldOfVision = 45)
-		#print intersections
-		isIntruderFound = False
+		
 		intersections = [(262, 214), (236.0, 288), (236.0, 301),
 		 (236.0, 305.0), (286.0, 305.0), 
 		 (286.0, 301), (286.0, 276)]
+
+	 	isIntruderFound = IsIntruderSeen(readable_path, intersections)
+	 	#isIntruderFound = False
 		intruderColor = (255,0,0)
 		if isIntruderFound:
 			intruderColor = (0,255,0)
@@ -220,7 +310,8 @@ def main():
 		#pygame.draw.circle(screen, (100,100,100), mouse, 5)
 
 		#isox,isoy = UpdateMovement(isox,isoy)
-		mouseClick = Update()
+		#mouseClick = Update()
+		Update()
 			
 		pygame.time.delay(10)
 
