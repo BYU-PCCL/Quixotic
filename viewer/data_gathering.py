cb2eeab56a1ae5771e350f629778dfc137d524b3
mirisr <irisrseaman@gmail.com>
2017-02-17 11:09:30 -0700

    added uav detection by intruder

--- viewer/data_gathering.py
+++ viewer/data_gathering.py
@@ -46,10 +46,10 @@ def Update():
 def load_polygons_here( fn="./info_gathering_paths.txt"):
     bdata = []
     for x in open( fn ):
-	        tmp = np.fromstring( x, dtype=float, sep=' ' )
-	        tmp = np.reshape( tmp/500, (-1,2) )
-	        tmp = np.vstack(( np.mean(tmp, axis=0, keepdims=True), tmp, tmp[0,:] ))
-	        bdata.append( tmp )
+        tmp = np.fromstring( x, dtype=float, sep=' ' )
+        tmp = np.reshape( tmp/500, (-1,2) )
+        tmp = np.vstack(( np.mean(tmp, axis=0, keepdims=True), tmp, tmp[0,:] ))
+        bdata.append( tmp )
     
 
     return bdata
@@ -87,102 +87,100 @@ def load_polygons( fn="./info_gathering_paths.txt" ):
 
 		polygonSegments.append(temp)
 
-
-	'''border'''
-	# polygonSegments.append([ 
-	# 	[ (0,0),(1000,0) ], 
-	# 	[ (1000,0),(1000,1000) ],
-	# 	[ (1000,1000), (0,1000)],
-	# 	[ (0,1000), (0,0) ]
-	# 	])
-        #print "toList:", toList
-	# for p in polygonSegments:
-	# 	print "\n", p
 	return polygonSegments
 
-def IsIntruderSeen(RRTPath, intersections):
+def IsUAVSeenByIntruder(uavLoc, radius, intended_path):
+	for point in intended_path:
+		distance = math.sqrt((uavLoc[0] - point[0]) ** 2 + (uavLoc[1] - point[1]) ** 2)
+		if distance <= radius:
+			return True, point
+	return False, None
+
+def IsIntruderSeenByUAV(RRTPath, intersections):
 
         # for point in RRTPath:
         #     isFound, loc = FindIntruderAtPoint(point, intersections)
-        #     print loc
         #     if isFound:
-        #         return True, loc
+        #     	intLoc = [int(loc[0]), int(loc[1])]
+        #         return True, intLoc # There's a bug ... breaks rrt
 
-        for i in xrange(1,len(RRTPath)):
-            segment = (RRTPath[i-1], RRTPath[i])
+    for i in xrange(1,len(RRTPath)):
+        segment = (RRTPath[i-1], RRTPath[i])
 
-            for j in xrange(1,len(intersections)):
-            	isovist_segment = (intersections[j-1], intersections[j])
-            	intersect, param = GetIntersection(segment, isovist_segment)
-            	
-            	if intersect != None:
-            		return True, RRTPath[i-1]
-            #Check the losing of the polygon segment
-            
-            isovist_segment = (intersections[0], intersections[-1])
-            intersect, param = GetIntersection(segment, isovist_segment)
-            if intersect != None:
-            	return True, intersections[0]
+        for j in xrange(1,len(intersections)):
+        	isovist_segment = (intersections[j-1], intersections[j])
+        	intersect, param = GetIntersection(segment, isovist_segment)
+        	
+        	if intersect != None:
+        		return True, RRTPath[i-1]
+        #Check the losing of the polygon segment
+        
+        isovist_segment = (intersections[0], intersections[-1])
+        intersect, param = GetIntersection(segment, isovist_segment)
+        if intersect != None:
+        	return True, intersections[0]
 
-        return False, None
+    return False, None
 
 def FindIntruderAtPoint(pos, intersections):
-        if intersections == []:
-            return False
-        points = intersections
-        cn = 0  # the crossing number counter
-        pts = points[:]
-        pts.append(points[0])
-        for i in range(len(pts) - 1):
-            if (((pts[i][1] <= pos[1]) and (pts[i+1][1] > pos[1])) or ((pts[i][1] > pos[1]) and (pts[i+1][1] <= pos[1]))):
-                    if (pos[0] < pts[i][0] + float(pos[1] - pts[i][1]) / (pts[i+1][1] - pts[i][1]) * (pts[i+1][0] - pts[i][0])):
-                            cn += 1
-        if bool(cn % 2)==1:
-            return True, pts[-1]
-        return False, None
+    if intersections == []:
+        return False
+    points = intersections
+    cn = 0  # the crossing number counter
+    pts = points[:]
+    pts.append(points[0])
+    collision = None
+    for i in range(len(pts) - 1):
+        if (((pts[i][1] <= pos[1]) and (pts[i+1][1] > pos[1])) or ((pts[i][1] > pos[1]) and (pts[i+1][1] <= pos[1]))):
+                if (pos[0] < pts[i][0] + float(pos[1] - pts[i][1]) / (pts[i+1][1] - pts[i][1]) * (pts[i+1][0] - pts[i][0])):
+                        cn += 1
+                        collision = pts[i]
+    if bool(cn % 2)==1:
+        return True, collision
+    return False, None
 
 def GetIntersection(ray, segment):
-        # RAY in parametric: Point + Direction * T1
-        r_px = ray[0][0]
-        r_py = ray[0][1]
+    # RAY in parametric: Point + Direction * T1
+    r_px = ray[0][0]
+    r_py = ray[0][1]
 
-        # direction
-        r_dx = ray[1][0] - ray[0][0]
-        r_dy = ray[1][1] - ray[0][1]
+    # direction
+    r_dx = ray[1][0] - ray[0][0]
+    r_dy = ray[1][1] - ray[0][1]
 
-        # SEGMENT in parametric: Point + Direction*T2
-        s_px = segment[0][0]
-        s_py = segment[0][1]
+    # SEGMENT in parametric: Point + Direction*T2
+    s_px = segment[0][0]
+    s_py = segment[0][1]
 
-        # direction
-        s_dx = segment[1][0] - segment[0][0]
-        s_dy = segment[1][1] - segment[0][1]
+    # direction
+    s_dx = segment[1][0] - segment[0][0]
+    s_dy = segment[1][1] - segment[0][1]
 
-        r_mag = math.sqrt(r_dx ** 2 + r_dy ** 2)
-        s_mag = math.sqrt(s_dx ** 2 + s_dy ** 2)
+    r_mag = math.sqrt(r_dx ** 2 + r_dy ** 2)
+    s_mag = math.sqrt(s_dx ** 2 + s_dy ** 2)
 
-        if r_mag == 0 or s_mag == 0:
-        	return None, None
-        # PARALLEL - no intersection
-        if (r_dx/r_mag) == (s_dx/s_mag):
-            if (r_dy/r_mag) == (s_dy/s_mag):
-                return None, None
-        
-        denominator = float( -s_dx*r_dy + r_dx*s_dy )
-        if denominator == 0:
+    if r_mag == 0 or s_mag == 0:
+    	return None, None
+    # PARALLEL - no intersection
+    if (r_dx/r_mag) == (s_dx/s_mag):
+        if (r_dy/r_mag) == (s_dy/s_mag):
             return None, None
-
-        T1 = (-r_dy * (r_px - s_px) + r_dx * ( r_py - s_py)) / denominator
-        T2 = (s_dx * ( r_py - s_py) - s_dy * ( r_px - s_px)) / denominator
-
-        if T1 >= 0 and T1 <= 1 and T2 >= 0 and T2 <= 1:
-            #Return the POINT OF INTERSECTION
-            x = r_px+r_dx*T2
-            y = r_py+r_dy*T2
-            param = T2
-            return ( x, y ), param
-
+    
+    denominator = float( -s_dx*r_dy + r_dx*s_dy )
+    if denominator == 0:
         return None, None
+
+    T1 = (-r_dy * (r_px - s_px) + r_dx * ( r_py - s_py)) / denominator
+    T2 = (s_dx * ( r_py - s_py) - s_dy * ( r_px - s_px)) / denominator
+
+    if T1 >= 0 and T1 <= 1 and T2 >= 0 and T2 <= 1:
+        #Return the POINT OF INTERSECTION
+        x = r_px+r_dx*T2
+        y = r_py+r_dy*T2
+        param = T2
+        return ( x, y ), param
+
+    return None, None
 
 
 def GetReadablePath(path, screen):
@@ -197,10 +195,9 @@ def GetReadablePath(path, screen):
 		readable_path.append(s_point)
 	return readable_path
 
-def DrawMiniIsovistForPath(path, screen, goal, intersection = None, reroute_color=None):
-	size = 55
+def DrawMiniIsovistForPath(path, screen, goal, iso_radius = 55, intersection = None, reroute_color=None):
 	if intersection != None:
-		pygame.draw.circle(screen, (0,255,255), intersection, size)
+		pygame.draw.circle(screen, (0,255,255), intersection, iso_radius)
 	# else:
 	# 	for i in xrange(1, len(path), 5):
 	# 		s_point = path[i-1]
@@ -208,6 +205,7 @@ def DrawMiniIsovistForPath(path, screen, goal, intersection = None, reroute_colo
 	caught_color = (255,0,0)
 	plan_color = (0,0,255)
 	color_in_use = caught_color
+	e_point = None
 	for i in xrange(1, len(path)):
 		s_point = path[i-1]
 		e_point = path[i]
@@ -217,8 +215,8 @@ def DrawMiniIsovistForPath(path, screen, goal, intersection = None, reroute_colo
 		if reroute_color != None:
 			color_in_use = reroute_color 
 		pygame.draw.line(screen, color_in_use, s_point, e_point, 2)
-		
-	pygame.draw.line(screen, color_in_use, e_point, goal, 2)
+	if e_point != None:
+		pygame.draw.line(screen, color_in_use, e_point, goal, 2)
 		
 
 '''
@@ -239,7 +237,7 @@ def main():
 	background = pygame.image.load(backgroundFileName)
 	background = pygame.transform.scale(background, (xdim, ydim))
 	backgroundRect = background.get_rect()
-
+	intruder_iso_radius = 70
 
 	
 
@@ -267,8 +265,6 @@ def main():
 	original_polygons = load_polygons_here()
 	X1, Y1, X2, Y2 = polygons_to_segments(original_polygons)
 
-	
-
 	Update()
 
 	isovist = iso.Isovist(polygonSegments)
@@ -282,12 +278,10 @@ def main():
 	# Get intended path
 	path = run_rrt( start, end, X1, Y1, X2, Y2)
 	intended_path = GetReadablePath(path, screen)
-	isIntruderFound, seen_loc = IsIntruderSeen(intended_path, intersections)
+	isIntruderFound, seen_loc = IsIntruderSeenByUAV(intended_path, intersections)
+	isUAVFound, u_seen_loc= IsUAVSeenByIntruder(UAVLocation, intruder_iso_radius, intended_path)
 
-	DrawMiniIsovistForPath(intended_path, screen, end_paint, intersection = seen_loc)
-
-	
-
+	DrawMiniIsovistForPath(intended_path, screen, end_paint, iso_radius= intruder_iso_radius, intersection = u_seen_loc)
 
 	intruderColor = (255,0,0)
 	if not isIntruderFound:
@@ -303,27 +297,52 @@ def main():
 	screen.blit(isovist_surface, isovist_surface.get_rect())
 
 	pygame.draw.circle(screen, (100,100,100), UAVLocation, 5)
-
 	pygame.draw.circle(screen, (0,255,0), start_paint, 7)
 	pygame.draw.circle(screen, (255,0,0), end_paint, 7)
-	if seen_loc != None:
-		pygame.draw.circle(screen, (255,255,0), seen_loc, 7)
+	
 
 	# Draw segments
 	for polygon in polygonSegments:
 		for segment in polygon:
 			pygame.draw.line(screen, (0, 0, 0), segment[0], segment[1] ,2)
 
-	if isIntruderFound:
+	if isUAVFound:
 		isovist = "262 214 236 260 286 260"
 
 		X1, Y1, X2, Y2 = polygons_to_segments(add_isovist_obstacle(isovist, original_polygons))
-		seen_rrt = np.atleast_2d( [(seen_loc[0]/500.0 ),(seen_loc[1]/500.0)] )
+		seen_rrt = np.atleast_2d( [(u_seen_loc[0]/500.0 ),(u_seen_loc[1]/500.0)] )
 		start = np.atleast_2d( [(101 /500.0 ) ,(455/500.0 )] )
 		path = run_rrt( seen_rrt, end, X1, Y1, X2, Y2)
 		rerouted_path = GetReadablePath(path, screen)
 
-		DrawMiniIsovistForPath(rerouted_path, screen, end_paint, reroute_color=(0, 255, 0))
+		DrawMiniIsovistForPath(rerouted_path, screen, end_paint, iso_radius= intruder_iso_radius, reroute_color=(0, 255, 0))
+
+	if seen_loc != None:
+		pygame.draw.circle(screen, (255,255,0), seen_loc, 5)
+	if u_seen_loc != None:
+		pygame.draw.circle(screen, (100,45,134), u_seen_loc, 5)
+
+
+	#write legend
+	pygame.draw.circle(screen, (100,100,100), [10,10], 5)
+	pygame.draw.circle(screen, (0,255,0), [10,40], 7)
+	pygame.draw.circle(screen, (255,0,0), [10,70], 7)
+	pygame.draw.circle(screen, (255,255,0), [10,100], 5)
+	pygame.draw.circle(screen, (100,45,134), [10,130], 5)
+
+	myfont = pygame.font.SysFont("comicsansms", 20)
+
+	# render text
+	label = myfont.render("UAV", 2, (0,0,0))
+	screen.blit(label, (30, 3))
+	label = myfont.render("start", 2, (0,0,0))
+	screen.blit(label, (30, 33))
+	label = myfont.render("goal", 2, (0,0,0))
+	screen.blit(label, (30, 63))
+	label = myfont.render("detection of intruder by UAV", 2, (0,0,0))
+	screen.blit(label, (30, 93))
+	label = myfont.render("detection of UAV by intruder", 2, (0,0,0))
+	screen.blit(label, (30, 123))
 
 	while True:
 		Update()
